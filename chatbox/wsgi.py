4fd8dffc08fa36b439ac9fd03005b365177f7885
Raj Kumar pegu <rajpegu894@gmail.com>
2026-06-13 02:39:41 +0530

    Update wsgi.py

--- chatbox/wsgi.py
+++ chatbox/wsgi.py
@@ -1,12 +1,6 @@
 import os
-from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 
-# 1. First, point Django to your settings file
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatbox.settings')
 
-# 3. Now that settings are loaded safely, trigger your database migrations
-call_command("migrate", interactive=False)
-
-# 4. Hand off the core process back to the standard Vercel live server instance
 application = get_wsgi_application()
