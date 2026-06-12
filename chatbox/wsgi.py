3f3223bcd54718e107bdca90eb601670a2da8121
Raj Kumar pegu <rajpegu894@gmail.com>
2026-06-13 02:19:38 +0530

    Update wsgi.py

--- chatbox/wsgi.py
+++ chatbox/wsgi.py
@@ -1,5 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
+call_command("migrate", interactive=False)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatbox.settings')
 
