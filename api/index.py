6c17b042ee3ed7904cfb4fa62c12cc11385dca04
Kamalpegu <rajpegu894@gmail.com>
2026-09-03 11:20:52 +0530

    Fix Django PATH_INFO resolution in Vercel WSGI entrypoint to eliminate 404 error

--- api/index.py
+++ api/index.py
@@ -1,4 +1,16 @@
-from a_core.wsgi import app
+from a_core.wsgi import application
 
-# Vercel entrypoint for Django WSGI application
-app = app
+class VercelWSGIMiddleware:
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        # Fix PATH_INFO on Vercel so Django receives user-requested route instead of /api/index.py
+        request_uri = environ.get('REQUEST_URI', '')
+        if request_uri:
+            environ['PATH_INFO'] = request_uri.split('?')[0]
+        elif environ.get('PATH_INFO', '').startswith('/api/index.py'):
+            environ['PATH_INFO'] = '/'
+        return self.app(environ, start_response)
+
+app = VercelWSGIMiddleware(application)
